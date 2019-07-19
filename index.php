9388999726603ebf2caac94ded011bdf57f74956
Salvador Hernández Salvatierra <astrargalar@gmail.com>
2019-07-19 19:02:16 +0200

    Ajustes al header.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<? php
+<?php
+get_header();
+?>
+<h1 class="alert alert-primary">Hola Estoy funcionando</h1>
+<div class="container">
+    <?php get_template_part('template-parts/main-loop'); ?>
+</div>
+<?php get_footer();
