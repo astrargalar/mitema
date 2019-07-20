dc23d43e2f18b98afba3600ddc920b5ee0fb0c9e
Salvador Hernández Salvatierra <astrargalar@gmail.com>
2019-07-20 14:09:25 +0200

    Ajustes de colores

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 get_header();
 ?>
-<h1 class="alert alert-primary">Hola Estoy funcionando</h1>
-<div class="container">
+
+<div class="container py-5">
     <?php get_template_part('template-parts/main-loop'); ?>
 </div>
 <?php get_footer();
